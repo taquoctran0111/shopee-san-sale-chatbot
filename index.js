8d056a6f5380cf2ba75c1bd31b06101846a47507
ta.quoctran <ta.quoctran@tap-ic.co.jp>
2026-07-10 21:46:47 +0700

    [upd] add index html

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
